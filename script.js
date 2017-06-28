08c6ca038839962dcdb97e362588bc7de9fef7e4
InnovateBham_16 <InnovateBham_16@Admins-iMac.local>
2017-06-28 16:55:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var button = document.createElement("button");
+button.innerHTML = "CLIQUE";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  alert("aint nobody fresher than my clique");
+});
+
+// text
+var name = lance
+$(document).on("click", function(e){
+    if (e.click) {
+        name.show();
+    } 
+});
